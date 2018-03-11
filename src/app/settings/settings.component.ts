7c8e9d2c83c0c3b55d18e03b51bdf7cb3bcc7e1e
Vlad Tkachov <vlad.miakshin@gmail.com>
2018-03-11 13:03:40 +0200

    ----

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -13,7 +13,8 @@ export class SettingsComponent implements OnInit {
   constructor(private commonService: CommonService) { }
 
   ngOnInit() {
-    this.getSettins()
+    this.getSettins();
+    document.getElementById("account").className="setting"
   }
 
   getSettins(){
@@ -22,5 +23,13 @@ export class SettingsComponent implements OnInit {
         this.settings = user.setings;
         console.log(this.settings);
       })
+  }
+
+  pick(event){
+    let divs = document.querySelectorAll("[class ^=setting]");
+    for(let div of divs){
+      div.className = "setting-hiden"
+    }
+    document.getElementById(event.target.name).className="setting"
   }
 }
